9fd4290d61485c900fea33334a5d30fa0466a70d
Philip Schatz <253202+philschatz@users.noreply.github.com>
2024-07-08 12:48:30 -0700

    adds vitest, a simple test, and vscode extension

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,10 @@ export default function Home() {
     return (
         <div className={styles.page}>
             <main className={styles.main}>
+                <h1 className={styles.title}>
+                    Welcome to <a href="https://nextjs.org">Next.js!</a>
+                </h1>
+
                 <Image
                     className={styles.logo}
                     src="/next.svg"
@@ -48,46 +52,19 @@ export default function Home() {
             </main>
             <footer className={styles.footer}>
                 <a
-                    href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+                    href="https://vercel.com"
                     target="_blank"
                     rel="noopener noreferrer"
                 >
-                    <Image
-                        aria-hidden
-                        src="/file-text.svg"
-                        alt="File icon"
-                        width={16}
-                        height={16}
-                    />
-                    Learn
-                </a>
-                <a
-                    href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <Image
-                        aria-hidden
-                        src="/window.svg"
-                        alt="Window icon"
-                        width={16}
-                        height={16}
-                    />
-                    Examples
-                </a>
-                <a
-                    href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <Image
-                        aria-hidden
-                        src="/globe.svg"
-                        alt="Globe icon"
-                        width={16}
-                        height={16}
-                    />
-                    Go to nextjs.org →
+                    Powered by Vercel{" "}
+                    <span className={styles.logo}>
+                        <Image
+                            src="/globe.svg"
+                            alt="Vercel Logo"
+                            width={72}
+                            height={16}
+                        />
+                    </span>
                 </a>
             </footer>
         </div>
