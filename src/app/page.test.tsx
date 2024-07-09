a50ccf4b01d30c59d8f7fcdf5d7cd01775072a0d
philschatz <253202+philschatz@users.noreply.github.com>
2024-07-09 16:34:02 +0000

    try to get eslint to format on save

--- src/app/page.test.tsx
+++ src/app/page.test.tsx
@@ -1,12 +1,29 @@
 import { expect, test } from "vitest";
-import { render, screen, within } from "@testing-library/react";
+import { render, 
+    
+
+
+    
+    
+    screen, within } from "@testing-library/react";
 import Home from "./page";
+
+import unused from "@testing-library/react"
+
+let foo = "";
+
+
+const f = () => {}
 
 test("Pages Router", () => {
     render(<Home />);
     const main = within(screen.getByRole("main"));
     expect(
-        main.getByRole("heading", { level: 1, name: /welcome to next\.js!/i }),
+        main.getByRole("heading",
+            
+            
+            
+            { level: 1, name: /welcome to next\.js!/i }),
     ).toBeDefined();
 
     const footer = within(screen.getByRole("contentinfo"));
