5717b57b101f960f08e3039f408474ec702d3cbc
Nathan Stitt <nathan@stitt.org>
2024-07-16 14:14:55 -0500

    move other links to left nav panel

--- src/components/app-layout.tsx
+++ src/components/app-layout.tsx
@@ -4,10 +4,14 @@ import { AppShell, Group  } from '@mantine/core';
 import { SafeInsightsLogo } from './si-logo'
 import { NavAuthMenu } from './nav-auth-menu';
 import Link from "next/link";
+import { NavigationPanel } from './navigation-panel';
 
 type Props = {
   children: React.ReactNode
 };
+
+
+
 
 export function AppLayout({ children}: Props) {
 
@@ -28,7 +32,7 @@ export function AppLayout({ children}: Props) {
                 </Group>
             </AppShell.Header>
 
-                <AppShell.Navbar p="md">Navbar</AppShell.Navbar>
+                <AppShell.Navbar p="md"><NavigationPanel /></AppShell.Navbar>
 
                 <AppShell.Main>{children}</AppShell.Main>
             </AppShell>
