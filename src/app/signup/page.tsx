96eb0027ab316192a9b442e5b6a41fd499fda84a
Chris Bendel <chrissbendel@gmail.com>
2024-07-17 12:46:55 -0400

    sign up working

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,20 +1,35 @@
 "use client";
-import styles from "../page.module.css";
-import {Button, Group, Loader, PasswordInput, TextInput, Title} from "@mantine/core";
+import {Button, Checkbox, Group, Loader, PasswordInput, Stack, Text, TextInput, Title} from "@mantine/core";
 import {useSignUp} from "@clerk/nextjs";
-import {isEmail, useForm} from "@mantine/form";
+import {isEmail, isNotEmpty, useForm} from "@mantine/form";
+import {redirect} from "next/navigation";
+import {useState} from "react";
+
+interface SignUpFormValues {
+    email: string;
+    password: string;
+    termsOfService: boolean;
+}
+
+interface EmailVerificationFormValues {
+    code: string;
+}
 
 export default function Signup() {
     const { isLoaded, signUp } = useSignUp();
+    const [verifying, setVerifying] = useState(false)
 
-    const form = useForm({
+    const form = useForm<SignUpFormValues>({
         initialValues: {
             email: '',
             password: '',
+            termsOfService: false
         },
 
         validate: {
-            email: isEmail('Invalid email')
+            email: isEmail('Invalid email'),
+            password: isNotEmpty('Required'),
+            termsOfService: isNotEmpty('You must accept terms of use')
         },
     });
 
@@ -22,14 +37,39 @@ export default function Signup() {
         // Add logic to handle loading state
         return <Loader />;
     }
-    console.log(signUp);
+
+    if (verifying) {
+        return <EmailVerificationStep />
+    }
+
+    if (signUp?.status === "complete") {
+        redirect("/")
+    }
+
+    const onSubmit = async (values: SignUpFormValues) => {
+        if (!isLoaded) return;
+
+        try {
+            await signUp.create({
+                emailAddressOrPhoneNumber: values.email,
+                password: values.password
+            })
+
+            await signUp.prepareEmailAddressVerification({
+                strategy: 'email_code',
+            });
+
+            setVerifying(true);
+        } catch {
+            console.error(JSON.stringify(err, null, 2));
+        }
+    }
 
     return (
-        <main className={styles.main}>
+        <Stack>
             <Title order={3}>Signup</Title>
-            <div>The current sign-up attempt status is {signUp.status}.</div>
 
-            <form onSubmit={form.onSubmit((values) => console.log(values))}>
+            <form onSubmit={form.onSubmit((values) => onSubmit(values))}>
                 <TextInput
                     withAsterisk
                     label="Email"
@@ -41,8 +81,64 @@ export default function Signup() {
                 <PasswordInput
                     withAsterisk
                     label="Password"
+                    width={400}
                     key={form.key('password')}
                     {...form.getInputProps('password')}
+                />
+
+                <Checkbox
+                    mt="md"
+                    label="I agree to the terms of service"
+                    key={form.key('termsOfService')}
+                    {...form.getInputProps('termsOfService', { type: 'checkbox' })}
+                />
+
+                <Group justify="flex-end" mt="md">
+                    <Button type="submit">Submit</Button>
+                </Group>
+            </form>
+        </Stack>
+    );
+}
+
+const EmailVerificationStep = () => {
+    const { isLoaded, signUp, setActive } = useSignUp();
+    const verifyForm = useForm<EmailVerificationFormValues>()
+
+    const handleVerify = async (values: EmailVerificationFormValues) => {
+        if (!isLoaded) return;
+        console.log(values);
+
+        try {
+            // Use the code the user provided to attempt verification
+            const completeSignUp = await signUp.attemptEmailAddressVerification({
+                code: values.code,
+            });
+
+            // If verification was completed, set the session to active
+            // and redirect the user
+            if (completeSignUp.status === 'complete') {
+                await setActive({ session: completeSignUp.createdSessionId });
+                redirect('/')
+            } else {
+                // If the status is not complete, check why. User may need to
+                // complete further steps.
+                console.error(JSON.stringify(completeSignUp, null, 2));
+            }
+        } catch (err: any) {
+            console.error('Error:', JSON.stringify(err, null, 2));
+        }
+    };
+
+    return (
+        <Stack>
+            <Text>We have emailed you a code, please enter that code here to finish signing up, thanks!</Text>
+            <form onSubmit={verifyForm.onSubmit((values) => handleVerify(values))}>
+                <TextInput
+                    withAsterisk
+                    label="Code"
+                    key={verifyForm.key('code')}
+                    {...verifyForm.getInputProps('code')}
                 />
 
 
@@ -50,6 +146,6 @@ export default function Signup() {
                     <Button type="submit">Submit</Button>
                 </Group>
             </form>
-        </main>
-    );
+        </Stack>
+    )
 }
