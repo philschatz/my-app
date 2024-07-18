675eded56f872e6de6eceae5bb1c87cb5e0ee6d4
Chris Bendel <chrissbendel@gmail.com>
2024-07-18 11:43:56 -0400

    Simple app

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+    baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -34,23 +34,25 @@ export default defineConfig({
   /* Configure projects for major browsers */
   projects: [
     {
-        name: 'global setup',
-        testMatch: /global\.setup\.ts/,
+      name: 'global setup',
+      testMatch: /global\.setup\.ts/,
     },
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
-        dependencies: ["global setup"],
+      dependencies: ["global setup"],
     },
 
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
+      dependencies: ["global setup"],
     },
 
     {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
+      dependencies: ["global setup"],
     },
 
     /* Test against mobile viewports. */
--- tests/app.spec.ts
+++ tests/app.spec.ts
@@ -2,28 +2,39 @@ import { setupClerkTestingToken } from "@clerk/testing/playwright";
 import { test, expect } from "@playwright/test";
 
 test.describe('app', () => {
-    test("sign up", async ({ page }) => {
+    test("unauthenticated user", async ({ page }) => {
         await setupClerkTestingToken({ page });
 
-        await page.goto("/sign-up");
-        await expect(page.locator("h1")).toContainText("Sign Up");
-        await page.waitForSelector('.cl-signUp-root', { state: 'attached' });
-        await page.locator('input[name=username]').fill('user' + Date.now());
-        await page.locator('input[name=password]').fill('Pass!@' + Date.now());
-        await page.getByRole('button', { name: 'Continue', exact: true }).click();
-        await page.waitForURL('**/protected');
+        await page.goto("/admin");
+        await expect(page).not.toHaveURL('/admin')
+        await expect(page).toHaveURL('/')
+        // await page.locator('input[name=identifier]').fill(process.env.E2E_CLERK_USER_USERNAME!);
+        // await page.getByRole('button', { name: 'Continue', exact: true }).click();
+        // await page.locator('input[name=password]').fill(process.env.E2E_CLERK_USER_PASSWORD!);
+        // await page.getByRole('button', { name: 'Continue', exact: true }).click();
+        // await page.waitForURL('**/protected');
     });
+    // test("sign up", async ({ page }) => {
+    //     await setupClerkTestingToken({ page });
+    //
+    //     await page.goto("/sign-up");
+    //     await expect(page.locator("h1")).toContainText("Sign Up");
+    //     await page.waitForSelector('.cl-signUp-root', { state: 'attached' });
+    //     await page.locator('input[name=username]').fill('user' + Date.now());
+    //     await page.locator('input[name=password]').fill('Pass!@' + Date.now());
+    //     await page.getByRole('button', { name: 'Continue', exact: true }).click();
+    //     await page.waitForURL('**/protected');
+    // });
 
-    test("sign in", async ({ page }) => {
-        await setupClerkTestingToken({ page });
-
-        await page.goto("/protected");
-        await expect(page.locator("h1")).toContainText("Sign In");
-        await page.waitForSelector('.cl-signIn-root', { state: 'attached' });
-        await page.locator('input[name=identifier]').fill(process.env.E2E_CLERK_USER_USERNAME!);
-        await page.getByRole('button', { name: 'Continue', exact: true }).click();
-        await page.locator('input[name=password]').fill(process.env.E2E_CLERK_USER_PASSWORD!);
-        await page.getByRole('button', { name: 'Continue', exact: true }).click();
-        await page.waitForURL('**/protected');
-    });
+    // test("sign in", async ({ page }) => {
+    //     await setupClerkTestingToken({ page });
+    //
+    //     await page.goto("/protected");
+    //     await expect(page.locator("h1")).toContainText("Sign In");
+    //     await page.locator('input[name=identifier]').fill(process.env.E2E_CLERK_USER_USERNAME!);
+    //     await page.getByRole('button', { name: 'Continue', exact: true }).click();
+    //     await page.locator('input[name=password]').fill(process.env.E2E_CLERK_USER_PASSWORD!);
+    //     await page.getByRole('button', { name: 'Continue', exact: true }).click();
+    //     await page.waitForURL('**/protected');
+    // });
 });
