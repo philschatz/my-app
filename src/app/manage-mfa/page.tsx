4cb7657b782237bd95449a021257c90fde19f527
Nathan Stitt <nathan@stitt.org>
2024-07-22 08:03:55 -0500

    finish MFA setup page

--- src/app/manage-mfa/page.tsx
+++ src/app/manage-mfa/page.tsx
@@ -1,161 +1,213 @@
-"use client";
+//"use client";
 
-import { useUser } from "@clerk/nextjs";
-import { TOTPResource } from "@clerk/types";
-import Link from "next/link";
-import * as React from "react";
-import {Button, Group, Stack, Text, TextInput, Title} from "@mantine/core";
-// import { QRCodeSVG } from "qrcode.react";
-// import { GenerateBackupCodes } from "../page";
+import { currentUser } from '@clerk/nextjs/server'
+import {IconLockAccess} from '@tabler/icons-react';
+// import { TOTPResource } from "@clerk/types";
+// import Link from "next/link";
 
-type AddTotpSteps = "add" | "verify" | "backupcodes" | "success";
+// import * as React from "react";
+// import { useForm, hasLength } from "@mantine/form";
 
-type DisplayFormat = "qr" | "uri";
+import { QRTotp, ValidateTotp } from './totp'
+import {Alert, Button, Flex, Skeleton, Stack, Text, TextInput, Title} from "@mantine/core";
+import { ErrorAlert } from '@/components/errors';
 
-function AddTotpScreen({
-    setStep,
-}: {
-    setStep: React.Dispatch<React.SetStateAction<AddTotpSteps>>;
-}) {
-    const { user } = useUser();
-    const [totp, setTOTP] = React.useState<TOTPResource | undefined>(undefined);
-    const [displayFormat, setDisplayFormat] =
-        React.useState<DisplayFormat>("qr");
 
-    React.useEffect(() => {
-        void user
-            ?.createTOTP()
-            .then((totp: TOTPResource) => {
-                setTOTP(totp);
-            })
-            .catch((err) =>
-                // See https://clerk.com/docs/custom-flows/error-handling
-                // for more info on error handling
-                console.error(JSON.stringify(err, null, 2)),
-            );
-    }, []);
+// https://clerk.com/docs/references/javascript/user/totp#totp-example
+
+// // import { GenerateBackupCodes } from "../page";
+
+
+// type AddTotpSteps = "add" | "verify" | "backupcodes" | "success";
+
+// type DisplayFormat = "qr" | "uri";
+
+
+
+// const ValidateCode = () => {
+
+
+//     return (
+//         <Stack>
+//             <Text>Once you have set up your authentication app, verify your code</Text>
+
+//             <Flex align="flex-end" justify="flex-start">
+
+//                 <TextInput
+//                     styles={{
+//                         input: {
+//                             width: "140px",
+//                         }
+//                     }}
+//                     type="text"
+//                     id="totp-code"
+//                     onChange={(e) => validateCode(e.currentTarget.value)}
+//                 />
+//                 <Button type="submit">Verify code</Button>
+
+//             </Flex>
+
+//         </Stack>
+//     );
+// }
+
+// function AddTotpScreen({
+//     setStep,
+// }: {
+//     setStep: React.Dispatch<React.SetStateAction<AddTotpSteps>>;
+// }) {
+//     const { user } = useUser();
+//     const [totp, setTOTP] = React.useState<TOTPResource | undefined>(undefined);
+//     const [displayFormat, setDisplayFormat] = React.useState<DisplayFormat>("qr");
+
+//     React.useEffect(() => {
+
+//         user?.createTOTP()
+//             .then((totp: TOTPResource) => {
+//                 setTOTP(totp);
+//             })
+//             .catch((err) => {
+//                 reportError(err)
+//             });
+
+//     }, []);
+
+//     if (!totp) return <Skeleton height={200} width={200} />;
+
+//     console.log(totp?.uri)
+
+//     const setCode = () => {
+
+//     }
+//     return (
+//         <Stack>
+//             <Title order={1}>Setup TOTP MFA</Title>
+
+
+//             <Text>
+//                 In order to use SafeInsights, you must enable multi-factor.
+//                 Please scan the QR code below with an authenticator app such as
+//                 Google Authenticator or Authy.
+//             </Text>
+
+
+//             <QRCodeSVG value={totp?.uri || ""} size={200} />
+
+//             <ValidateCode />
+
+//         </Stack>
+//     );
+// }
+
+// function VerifyTotpScreen({
+//     setStep,
+// }: {
+//     setStep: React.Dispatch<React.SetStateAction<AddTotpSteps>>;
+// }) {
+//     const { user } = useUser();
+//     const [code, setCode] = React.useState("");
+//     const form = useForm({
+//         initialValues: {
+//             code: ""
+//         },
+//         validate: {
+//             code: hasLength(6)
+//         }
+//      });
+//     const onSubmit = form.onSubmit(async (values) => {
+
+//     }))
+//     const verifyTotp = async (e: React.FormEvent) => {
+//         e.preventDefault();
+//         try {
+//             await user?.verifyTOTP({ code });
+//             setStep("backupcodes");
+//         } catch (err) {
+//             console.error(JSON.stringify(err, null, 2));
+//         }
+//     };
+
+//     return (
+//         <Stack>
+//             <NumberInput order={1}>Verify TOTP</Title>
+//             <form onSubmit={(e) => verifyTotp(e)}>
+//                 <Stack>
+//                     <Button onClick={() => setStep("add")}>Reset</Button>
+//                 </Stack>
+//             </form>
+//         </Stack>
+//     );
+// }
+
+// function BackupCodeScreen({
+//     setStep,
+// }: {
+//     setStep: React.Dispatch<React.SetStateAction<AddTotpSteps>>;
+// }) {
+//     return (
+//         <Stack>
+//             <Title order={1}>Backup codes</Title>
+//             <Text>
+//                 Save this list of backup codes somewhere safe in case you
+//                 need to access your account in an emergency
+//             </Text>
+//             {/* <GenerateBackupCodes /> */}
+//             <Button onClick={() => setStep("success")}>
+//                 Finish
+//             </Button>
+//         </Stack>
+//     );
+// }
+
+// function SuccessScreen() {
+//     return (
+//         <Stack>
+//             <Title order={1}>Success!</Title>
+//             <Text>You have successfully added TOTP MFA via an authentication application.</Text>
+//         </Stack>
+//     );
+// }
+
+
+
+export default async function AddMFaScreen() {
+    //const [step, setStep] = React.useState<AddTotpSteps>("add");
+    //const { isLoaded, user } = useUser();
+    const user = await currentUser()
+
+    console.log(user)
+
+    if (!user) return <ErrorAlert title="Access Denied" message="You must be logged in to access this page" icon={<IconLockAccess />} />
+    if (user.totpEnabled) return <ErrorAlert title="MFA already enabled" message="You have already enabled MFA on your account" />
+
+    // const { user: u } = useUser()
+    // console.log(u)
+    // //
+    //if (!isLoaded) return null;
+
+    // if (!user) {
+    //     return <Text>You must be logged in to access this page</Text>;
+    // }
 
     return (
-        <Stack>
-            <Title order={1}>Add TOTP MFA</Title>
-
-            {totp && displayFormat === "qr" && (
-                <Group>
-                    {/* <QRCodeSVG value={totp?.uri || ""} size={200} /> */}
-                    <Button onClick={() => setDisplayFormat("uri")}>
-                        Use URI instead
-                    </Button>
-                </Group>
-            )}
-            {totp && displayFormat === "uri" && (
-                <Group>
-                    <Text>
-                        {totp.uri}
-                    </Text>
-                    <Button onClick={() => setDisplayFormat("qr")}>
-                        Use QR Code instead
-                    </Button>
-                </Group>
-            )}
-            <Group>
-                <Button onClick={() => setStep("add")}>
-                    Reset
-                </Button>
-            </Group>
-
-
-            <Stack>
-                <Text>Once you have set up your authentication app, verify your code</Text>
-                <Button onClick={() => setStep("verify")}>Verify</Button>
-            </Stack>
-
-        </Stack>
-    );
-}
-
-function VerifyTotpScreen({
-    setStep,
-}: {
-    setStep: React.Dispatch<React.SetStateAction<AddTotpSteps>>;
-}) {
-    const { user } = useUser();
-    const [code, setCode] = React.useState("");
-
-    const verifyTotp = async (e: React.FormEvent) => {
-        e.preventDefault();
-        try {
-            await user?.verifyTOTP({ code });
-            setStep("backupcodes");
-        } catch (err) {
-            console.error(JSON.stringify(err, null, 2));
-        }
-    };
-
-    return (
-        <Stack>
-            <Title order={1}>Verify TOTP</Title>
-            <form onSubmit={(e) => verifyTotp(e)}>
-                <Stack>
-                    <TextInput
-                        type="text"
-                        id="totp-code"
-                        label='Enter the code from your authentication app'
-                        onChange={(e) => setCode(e.currentTarget.value)}
-                    />
-                    <Button type="submit">Verify code</Button>
-                    <Button onClick={() => setStep("add")}>Reset</Button>
-                </Stack>
-            </form>
-        </Stack>
-    );
-}
-
-function BackupCodeScreen({
-    setStep,
-}: {
-    setStep: React.Dispatch<React.SetStateAction<AddTotpSteps>>;
-}) {
-    return (
-        <Stack>
-            <Title order={1}>Backup codes</Title>
+        <Flex direction="column" align="center" justify="center" gap="lg">
+            <Title order={1}>Setup TOTP MFA</Title>
             <Text>
-                Save this list of backup codes somewhere safe in case you
-                need to access your account in an emergency
+                In order to use SafeInsights, you must enable multi-factor authentication.
+                Please scan the QR code below with an authenticator app such as
+                Google Authenticator or Authy.
             </Text>
-            {/* <GenerateBackupCodes /> */}
-            <Button onClick={() => setStep("success")}>
-                Finish
-            </Button>
-        </Stack>
-    );
-}
 
-function SuccessScreen() {
-    return (
-        <Stack>
-            <Title order={1}>Success!</Title>
-            <Text>You have successfully added TOTP MFA via an authentication application.</Text>
-        </Stack>
-    );
-}
+            <QRTotp />
 
-export default function AddMFaScreen() {
-    const [step, setStep] = React.useState<AddTotpSteps>("add");
-    const { isLoaded, user } = useUser();
+            <Text>
+                Once you have set up your authentication app, verify your code
+                by inputting it's current value below.
+            </Text>
 
-    if (!isLoaded) return null;
+            <ValidateTotp />
 
-    if (!user) {
-        return <Text>You must be logged in to access this page</Text>;
-    }
+        </Flex>
+    )
 
-    return (
-        <>
-            {step === "add" && <AddTotpScreen setStep={setStep} />}
-            {step === "verify" && <VerifyTotpScreen setStep={setStep} />}
-            {step === "backupcodes" && <BackupCodeScreen setStep={setStep} />}
-            {step === "success" && <SuccessScreen />}
-            <Link href="/manage-mfa">Manage MFA</Link>
-        </>
-    );
 }
