c02f6010bd6eac2fd4f280c9f0dda090b9f54064
Nathan Stitt <nathan@stitt.org>
2024-07-22 08:04:26 -0500

    add login/signup components

---
+++ src/components/errors.tsx
@@ -0,0 +1,48 @@
+import { notifications } from "@mantine/notifications";
+import { Alert, AlertProps } from '@mantine/core'
+import { IconAlertTriangle } from '@tabler/icons-react'
+
+type ClerkAPIErrorResponse = {
+    errors: Array<{
+        code: string
+        message: string
+        long_message: string
+    }>
+}
+
+export function isClerkApiError(error: any): error is ClerkAPIErrorResponse {
+    return (typeof error == 'object' && Array.isArray(error.errors) && error.errors?.[0].code)
+}
+
+export const reportError = (error: any, title = 'An error occured') => {
+
+    const message = isClerkApiError(error) ?
+        error.errors.map((e: any) => `${e.message}: ${e.longMessage}`).join('\n')
+        : JSON.stringify(error, null, 2)
+
+    console.error('Error:', message);
+
+    notifications.show({
+        color: "red",
+        title,
+        message,
+    });
+}
+
+type ErrorAlertProps = { message: string } & AlertProps
+
+export const ErrorAlert: React.FC<ErrorAlertProps> = ({
+    icon = <IconAlertTriangle />,
+    title = 'An error occured',
+    message
+}) => {
+
+    return (
+        <Alert
+            variant="light"
+            color="red"
+            title={title}
+            icon={icon}
+        >{ message }</Alert >
+    )
+}
