bc665477dd2315d1b2d3aac488387e33402e8147
Nathan Stitt <nathan@stitt.org>
2024-07-22 15:57:19 -0500

    add lint and typecheck to workflow

--- .babelrc.js
+++ .babelrc.js
@@ -1,13 +1,22 @@
 const path = require("path");
-module.exports = {
+module.exports = api => ({
     presets: ["next/babel"],
     plugins: [
+        [
+            "module-resolver", {
+                "root": ["./src"],
+                "alias": {
+                    "^@/(.+)": "./src/\\1",
+                }
+            }
+        ],
         [
             "@stylexjs/babel-plugin",
             // See all options in the babel plugin configuration docs:
             // https://stylexjs.com/docs/api/configuration/babel-plugin/
             {
-                dev: process.env.NODE_ENV === "development",
+                dev: api.env("development"),
+                test: api.env("test"),
                 genConditionalClasses: true,
                 treeshakeCompensation: true,
                 aliases: {
@@ -20,4 +29,4 @@ module.exports = {
             },
         ],
     ],
-};
+})
