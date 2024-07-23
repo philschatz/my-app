0418921f1dff06cfaefddd221d50cb8744d60a7b
Nathan Stitt <nathan@stitt.org>
2024-07-23 12:28:27 -0500

    replace jest with vite

--- .babelrc.js
+++ .babelrc.js
@@ -11,23 +11,5 @@ module.exports = {
                 },
             },
         ],
-        [
-            "@stylexjs/babel-plugin",
-            // See all options in the babel plugin configuration docs:
-            // https://stylexjs.com/docs/api/configuration/babel-plugin/
-            {
-                dev: process.env.NODE_ENV == "development",
-                test: process.env.NODE_ENV == "test",
-                genConditionalClasses: true,
-                treeshakeCompensation: true,
-                aliases: {
-                    "@/*": [path.join(__dirname, "*")],
-                },
-                unstable_moduleResolution: {
-                    type: "commonJS",
-                    rootDir: path.join(__dirname, "../.."),
-                },
-            },
-        ],
     ],
 };
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -2,9 +2,6 @@
 
 import { SignUp } from "@/components/signup";
 
-
 export default function SignUpPage() {
-  return (
-    <SignUp />
-  );
+    return <SignUp />;
 }
