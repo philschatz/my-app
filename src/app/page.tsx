8448c546ee85bc3f6c5ad1234d9ad3f57a3c44c4
Nathan Stitt <nathan@stitt.org>
2024-07-23 10:15:26 -0500

    remove duplicated code from signup page, use component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import { SignedIn, SignedOut } from "@clerk/nextjs";
-import { Text } from "@mantine/core";
 import { LoginOrSignup } from "@/components/login-or-signup";
 import Link from "next/link";
 import * as stylex from "@stylexjs/stylex";
