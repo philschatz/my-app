a5a3e4ffa3d532e0b4e16b5ec545983488383897
philschatz <253202+philschatz@users.noreply.github.com>
2024-07-24 18:57:04 +0000

    :skull: this commit breaks

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,7 +1,9 @@
 import { ClerkProvider } from "@clerk/nextjs";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { MantineProvider } from "@mantine/core";
 import { theme } from "./theme";
 import { ModalsProvider } from "@mantine/modals";
+import AppContext from "@/AppContext";
 
 type Props = {
     children: React.ReactNode;
@@ -11,7 +13,9 @@ export const Providers: React.FC<Props> = ({ children }) => {
     return (
         <MantineProvider theme={theme}>
             <ModalsProvider>
-                <ClerkProvider>{children}</ClerkProvider>
+                <AppContext.Provider value={{ SignedIn, SignedOut }}>
+                    <ClerkProvider>{children}</ClerkProvider>
+                </AppContext.Provider>
             </ModalsProvider>
         </MantineProvider>
     );
