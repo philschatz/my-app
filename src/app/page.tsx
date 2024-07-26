67000093b2b3bf9a3f96e7ba402d3f931c09121a
Philip Schatz <253202+philschatz@users.noreply.github.com>
2024-07-26 15:16:43 -0700

    :shirt: typo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { LoginOrSignup } from "@/components/login-or-signup";
 import Link from "next/link";
 import { Flex } from "@mantine/core";
-import { container } from "./styles.css.ts";
+import { container } from "./styles.css";
 
 export default function Home() {
     return (
